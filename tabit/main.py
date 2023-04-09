6266ddee80080fd93fd4f38add67bb5b216fb8be
Suzuto Kirishima <100030764+trwrup@users.noreply.github.com>
2023-04-09 18:03:10 +0900

    add: 初期状態

---
+++ tabit/main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+import pandas as pd
+import numpy as np
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html", title="Home", h2="Home")
+
+@app.route("/member")
+def member():
+    return render_template("member.html", title="メンバ〜", h2="メンバ〜")
+
+@app.route("/roulette")
+def roulette():
+    df = pd.read_csv("./tarbitte/static/csv/pref_data.csv")
+    df = df.to_numpy().tolist()
+    return render_template("roulette.html", title="る〜れっと", h2="る〜れっと", df=df)
+
+@app.route("/info/<int:id>")
+def info(id):
+    id = id-1
+    df = pd.read_csv("./tarbitte/static/csv/pref_data.csv")
+    df = df.to_numpy().tolist()
+    return render_template("info.html", title="都道府県情報", h2=f"{df[id][1]}の情報", id=df[int(id)])
